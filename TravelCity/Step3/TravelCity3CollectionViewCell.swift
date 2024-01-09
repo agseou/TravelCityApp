b0a474142aa28024567c35d719f7af1e9ccdbb27
SeoU <tjdnfkd1225@naver.com>
2024-01-09 22:32:22 +0900

    [Step3] SupplementaryView 제외 구현

---
+++ TravelCity/Step3/TravelCity3CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCity3CollectionViewCell.swift
+//  TravelCity
+//
+//  Created by 은서우 on 2024/01/09.
+//
+
+import UIKit
+
+class TravelCity3CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
